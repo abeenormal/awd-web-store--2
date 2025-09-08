59e43cb5480afca2394edaed61c21c9154495db3
hickmb34@gmail.com <hickmb34@gmail.com>
2025-09-08 21:43:54 +0000

    Edited Base, Courses, Home, MyCourses and 1 more

--- client_code/MyCourses/__init__.py
+++ client_code/MyCourses/__init__.py
@@ -1,5 +1,12 @@
 from ._anvil_designer import MyCoursesTemplate
 from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.facebook.auth
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
 
 
 class MyCourses(MyCoursesTemplate):

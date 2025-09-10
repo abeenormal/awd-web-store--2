54e89bc9a80a3138f11c1ce47531a2a655035593
hickmb34@gmail.com <hickmb34@gmail.com>
2025-09-10 01:13:28 +0000

    Edited settings and Base

--- client_code/Base/__init__.py
+++ client_code/Base/__init__.py
@@ -13,55 +13,52 @@ from ..MyCourses import MyCourses
 class Base(BaseTemplate):
   def __init__(self, **properties):
 
-    
-    
+    user = anvil.users.get_user()
+    if user:
+      self.sign_in.text = f"Welcome, {user[email]}"
+      self.log_out_button.visible = True
 
-    
-    
+   
+
     # Set Form properties and Data Bindings.
 
-    self.init_components(**properties)
+    self.init_components(**properties) 
+
+  
+    
+    
+    self.content_panel.add_component(Home())
 
   
 
- 
-    # Set the visibility of the link named "my_courses_link"
-    user = anvil.users.get_user()
 
-    if user:
-      self.sign_in.text = "Welcome"
-      self.my_courses_link.vis
-        
-    else:
-       self.my_courses_link.visible = False   
-
-    self.content_panel.add_component(Home())
-   
 
 # Any code you write here will run before the form opens.
 
-
+    
 
   def go_to_home(self):
-   self.content_panel.clear()
-   self.content_panel.add_component(Home())
+    """This method is called """
+    self.content_panel.clear()
+    self.content_panel.add_component(Home())
 
   def title_click(self, **event_args):
     """This method is called when the link is clicked"""
-    self.go_to_home() 
-
-  def my_courses_link_click(self, **event_args): 
-    """This method is called when the link is clicked"""
-    self.content_panel.clear()
-    self.content_panel.add_component(MyCourses())
+    self.go_to_home()
 
   def sign_in_click(self, **event_args):
-   """This method is called when the link is clicked"""
-   anvil.users.login_with_form()
-
-  
-   
+    """This method is called when the link is clicked"""
+    anvil.users.login_with_form()
     
-  
-
+  def log_out_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.users.logout()
+    anvil.open_form('Base')
     
+
+
+
+
+
+
+

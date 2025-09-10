f08ac78338fb130cfcb85bed8b6acc70e0f322fa
hickmb34@gmail.com <hickmb34@gmail.com>
2025-09-10 03:08:47 +0000

    Edited Base

--- client_code/Base/__init__.py
+++ client_code/Base/__init__.py
@@ -12,17 +12,16 @@ from ..MyCourses import MyCourses
 
 class Base(BaseTemplate):
   def __init__(self, **properties):
-
-    user = anvil.users.get_user()
-    if user:
-      self.sign_in.text = f"Welcome, {user[email]}"
-      self.log_out_button.visible = True
-
+  
+    
+    
    
 
     # Set Form properties and Data Bindings.
 
-    self.init_components(**properties) 
+    self.init_components(**properties)
+   
+   
 
   
     
@@ -35,7 +34,13 @@ class Base(BaseTemplate):
 
 # Any code you write here will run before the form opens.
 
-    
+  def change_sign_in_text(self):
+      user = anvil.users.get_user()
+      if user:
+        email = user["email"]
+        self.sign_in.text = email
+      else:
+        self.sign_in.text = "Sign in" 
 
   def go_to_home(self):
     """This method is called """
@@ -49,11 +54,31 @@ class Base(BaseTemplate):
   def sign_in_click(self, **event_args):
     """This method is called when the link is clicked"""
     anvil.users.login_with_form()
+    self.change_sign_in_text()
+    self.update_login_ui()
     
   def log_out_button_click(self, **event_args):
     """This method is called when the button is clicked"""
     anvil.users.logout()
     anvil.open_form('Base')
+
+  def update_login_ui(self):
+    user = anvil.users.get_user()
+    if user:
+     self.log_out_button.visible = True 
+     self.my_course.visible = True
+     self.sign_in.visible = False
+    else:
+     self.log_out_button.visible = False
+     
+
+  def my_course_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.content_panel.clear()
+    self.content_panel.add_component(MyCourses())
+    
+
+ 
     
 
 

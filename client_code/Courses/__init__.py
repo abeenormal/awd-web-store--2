1ba84f024573640ab05869e8b06d643526e3353d
hickmb34@gmail.com <hickmb34@gmail.com>
2025-09-11 21:57:07 +0000

    Edited Courses and CourseItem

--- client_code/Courses/__init__.py
+++ client_code/Courses/__init__.py
@@ -16,8 +16,7 @@ class Courses(CoursesTemplate):
 
   
     # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-    
+    self.init_components(**properties)    
     self.load_courses()
    
      # Any code you write here will run before the form opens.
@@ -26,12 +25,17 @@ class Courses(CoursesTemplate):
     course_panel = GridPanel()
 
     for i, course in enumerate(courses):
-     c = CourseItem(name=course["name"], button_text=f"Purchase for ${course['price']}", description=course["description"], image=course["image"], button_callback=None)  
+      c = CourseItem(name=course["name"], button_text=f"Purchase for ${course['price']}", description=course["description"], image=course["image"], button_callback=None)
     course_panel.add_component(c, row=str(i//2), width_xs=6)
-      
+  
+    
+    
+    
+   
+   
     self.content_panel.add_component(course_panel)
     
-    for course in courses:
-     c = CourseItem(name=course["name"], button_text=f"Purchase for ${course['price']}", description=course["description"], image=course["image"], button_callback=None)  
-    course_panel.add_component(c, row=str(i//2), width_xs=6)
-    
+      
+   
+
+  

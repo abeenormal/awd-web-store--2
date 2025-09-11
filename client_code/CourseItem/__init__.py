b070ead3ca90572e29b24e3f2658c7e94f2f6a17
hickmb34@gmail.com <hickmb34@gmail.com>
2025-09-11 19:59:32 +0000

    Created CourseItem; Edited Courses

---
+++ client_code/CourseItem/__init__.py
@@ -0,0 +1,28 @@
+from ._anvil_designer import CourseItemTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.facebook.auth
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class CourseItem(CourseItemTemplate):
+  def __init__(self, name, description, button_text, image, button_callback, **properties):
+
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    self.name_label.content = name
+    self.description_label.content = description
+    self.button.text = button_text
+    self.image_content.source = image
+    
+
+    
+    
+   
+
+    # Any code you write here will run before the form opens.
--- client_code/Courses/__init__.py
+++ client_code/Courses/__init__.py
@@ -8,11 +8,30 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.users
+from ..CourseItem import CourseItem
 
 
 class Courses(CoursesTemplate):
   def __init__(self, **properties):
+
+  
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    
+    self.load_courses()
+   
+     # Any code you write here will run before the form opens.
+  def load_courses(self):
+    courses = anvil.server.call("get_course_details").search()
+    course_panel = GridPanel()
 
-    # Any code you write here will run before the form opens.
+    for i, course in enumerate(courses):
+     c = CourseItem(name=course["name"], button_text=f"Purchase for ${course['price']}", description=course["description"], image=course["image"], button_callback=None)  
+    course_panel.add_component(c, row=str(i//2), width_xs=6)
+      
+    self.content_panel.add_component(course_panel)
+    
+    for course in courses:
+     c = CourseItem(name=course["name"], button_text=f"Purchase for ${course['price']}", description=course["description"], image=course["image"], button_callback=None)  
+    course_panel.add_component(c, row=str(i//2), width_xs=6)
+    

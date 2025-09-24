64a79eea3304539cdc4e79e336ce913e7588c9af
hickmb34@gmail.com <hickmb34@gmail.com>
2025-09-24 14:18:48 +0000

    Edited Checkout

--- client_code/Checkout/__init__.py
+++ client_code/Checkout/__init__.py
@@ -14,16 +14,16 @@ import stripe
 
 class Checkout(CheckoutTemplate):
   def __init__(self,id_name, button_callback, **properties):
-    
+
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.update_form(id_name)
-    
-    
-  
-   
+
+
+
+
     # Any code you write here will run before the form opens.
-    
+
 
   def update_form(self,id_name):
     course = anvil.server.call('get_course_details', id_name)
@@ -32,29 +32,30 @@ class Checkout(CheckoutTemplate):
     self.description_label.text = course['description']
     self.price_label.text = f"${course['price']} USD"
     self.image_content.source = course['image']
-  
-    
-   
+
+
+
 
   def back_button_click(self, **event_args):
     """This method is called when the button is clicked"""
     self.back_button_callback()
-    
+
 
   def buy_click(self, **event_args):
     """This method is called when the button is clicked"""
   if anvil.users.get_user() is None:
     anvil.users.login_with_form()
-    
+
     user = anvil.users.get_user()
     if user is None:    
-     alert("Please sign in!")
-     return
-    
+      alert("Please sign in!")
+      
+    return
+
   if user["purchased_courses"] and self.course["id_name"]in user["purchased_courses"]:
     alert("You already own this course!")
     return
-    
+
     token, info=stripe.checkout.get_token(amount= self.course["price"]*100, currency="USD",title=self.course["name"], description=self.course["description"])
     try:
       anvil.server.call("charge_user", token, user["email"], self.course["id_name"])
